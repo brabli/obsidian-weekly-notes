96f8bb30be3f703904675b7ac0d030a82e4fb4d1
brabli <67018167+brabli@users.noreply.github.com>
2026-01-08 21:42:17 +0000

    Update function to replace template vars with custom format strings

--- src/utils.ts
+++ src/utils.ts
@@ -2,18 +2,19 @@ import type { App } from "obsidian";
 import { moment } from "obsidian";
 
 /**
- * Uses core "templates" plugin option to replace the following variables in the template:
+ * Replace Obsidian template variables using settings from core Templates plugin if set.
  *
  * {{title}} - The title of the new file
  * {{date}} - The current date
  * {{time}} - The current time
  *
- * @param app - The Obsidian App instance
+ * @param app             - The Obsidian App instance
  * @param templateContent - The content of the template file
- * @param title - The title of the new file
+ * @param title           - The title of the new file
+ *
  * @returns The template content with the variables replaced
  */
-export async function replaceFileVars(
+export async function replaceTemplateVariables(
     app: App,
     templateContent: string,
     title: string,
@@ -38,11 +39,23 @@ export async function replaceFileVars(
 
     let dateFormat = "YYYY-MM-DD";
     let timeFormat = "HH:mm";
-    if (coreTemplatesConfig?.dateFormat) {
+
+    if (coreTemplatesConfig.timeFormat) {
+        timeFormat = coreTemplatesConfig.timeFormat;
+    }
+
+    if (coreTemplatesConfig.dateFormat) {
         dateFormat = coreTemplatesConfig.dateFormat;
     }
-    if (coreTemplatesConfig?.timeFormat) {
-        timeFormat = coreTemplatesConfig.timeFormat;
+
+    // Handle one-off format strings such as {{date:YYYY-[W]WW}} or {{time:HH}}
+    // Docs: https://help.obsidian.md/plugins/templates#Template+variables
+    const matches = templateContent.matchAll(/{{(date|time):(.*)}}/g);
+
+    for (const match of matches) {
+        const fullMatch = match[0];
+        const formatString = match[2];
+        templateContent = templateContent.replace(fullMatch, moment().format(formatString));
     }
 
     templateContent = templateContent.replace(/{{date}}/g, moment().format(dateFormat));
